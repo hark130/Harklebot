2a740bc3555723c06ff1ba4b2217f2e75909b1f7
Joseph Harkleroad <hark130@yahoo.com>
2017-03-14 11:05:31 -0500

    Update Template_Scraper.py
    Updated comment block numbering based on new code block that was inserted previously.

--- Scrapers/Template_Scraper.py
+++ Scrapers/Template_Scraper.py
@@ -373,28 +373,28 @@ while True:
 
             
 #    print("\nFetching First URL:") # DEBUGGING
-    # 4. FIND THE FIRST URL
+    # 5. FIND THE FIRST URL
     # NEW PROCEDURE FOR VERSION 1-2
     # find_a_URL(htmlString, [searchPhrase], [searchStart], [searchEnd])
 
-    ## 4.1. Find the first URL if there isn't already one
+    ## 5.1. Find the first URL if there isn't already one
     if firstURL.__len__() == 0 and firstSearchPhrase.__len__() > 0:
         firstURL = find_a_URL(comicContentDecoded, firstSearchPhrase, 'href="', '"')
 
         firstURL = firstURL.replace('"', '') # find_a_URL() leaves the [searchEnd] on the return value
 
-    ## 4.2. Validate findings
-    ### 4.2.1. firstURL empty and this is the first stop
+    ## 5.2. Validate findings
+    ### 5.2.1. firstURL empty and this is the first stop
     if firstURL.__len__() == 0 and (currentURL == targetComicURL or currentURL == latestURL): # Only check on first run
-        #### 4.2.1.1. Check for search criteria... Sometimes, there's no "First URL" to find... Only print on first run
+        #### 5.2.1.1. Check for search criteria... Sometimes, there's no "First URL" to find... Only print on first run
         if firstSearchPhrase.__len__() == 0: # and firstURL.__len__() == 0:
             print("First URL search criteria not configured.") # DEBUGGING  
-        #### 4.2.2.1. If there's a search criteria configured but we didn't find a firstURL, something went wrong(?)
+        #### 5.2.2.1. If there's a search criteria configured but we didn't find a firstURL, something went wrong(?)
         else:
             print("First URL Not found with search criteria:\t{}".format(firstSearchPhrase)) # DEBUGGING  
-    ### 4.2.2. Found firstURL on the first stop
+    ### 5.2.2. Found firstURL on the first stop
     elif firstURL.__len__() > 0 and (currentURL == targetComicURL or currentURL == latestURL): # Found it first time
-        #### 4.2.2.1. Ensure the firstURL is an absolute URL
+        #### 5.2.2.1. Ensure the firstURL is an absolute URL
         try:
             firstURL = make_rel_URL_abs(baseURL, firstURL)
         except Exception as err:
@@ -404,7 +404,7 @@ while True:
         else:
             print("First URL:\t{}".format(firstURL)) # DEBUGGING    
 
-        #### 4.2.2.2. Ensure the firstURL is valid
+        #### 5.2.2.2. Ensure the firstURL is valid
         try:
             if is_URL_valid(firstURL) is False:
                 print("Invalid URL:\t{}".format(firstURL)) # DEBUGGING
@@ -414,20 +414,20 @@ while True:
             sys.exit()
 
 
-    # 5. FIND THE IMAGE .GIF
+    # 6. FIND THE IMAGE .GIF
     # NEW PROCEDURE FOR VERSION 1-2
     # find_a_URL(htmlString, [searchPhrase], [searchStart], [searchEnd])
 #    print("\nFetching Image URL:")
     imageURL = find_a_URL(comicContentDecoded, imageSearchPhrase, imageBeginPhrase, validFileTypeList)               
 
 
-    # 6. CHANGE RELATIVE URLS TO ABSOLUTE URLS
+    # 7. CHANGE RELATIVE URLS TO ABSOLUTE URLS
     if imageURL.__len__() > 0:
-        ## 6.1. Clean up any URLs that begin with '//' because Request() doesn't like them
+        ## 7.1. Clean up any URLs that begin with '//' because Request() doesn't like them
         if imageURL.find('//') == 0:
             imageURL = 'http:' + imageURL
             
-        ## 6.2. Ensure the imageURL is an absolute URL
+        ## 7.2. Ensure the imageURL is an absolute URL
         try:
             imageURL = make_rel_URL_abs(baseURL, imageURL)
         except Exception as err:
@@ -438,7 +438,7 @@ while True:
 #            print("Image URL:\t{}".format(imageURL)) # DEBUGGING
             pass
 
-        ## 6.3. Ensure URL is valid
+        ## 7.3. Ensure URL is valid
         try:
             if is_URL_valid(imageURL) is False:
                 print("Invalid URL:\t{}".format(imageURL)) # DEBUGGING
@@ -450,7 +450,7 @@ while True:
         print("Did not find an image URL!")
         sys.exit()
 
-    # 7. DETERMINE THE IMAGE FILE EXTENSION
+    # 8. DETERMINE THE IMAGE FILE EXTENSION
     if imageURL.__len__() > 0:
         for extension in validFileTypeList:
             if imageURL.find(extension) == (imageURL.__len__() - extension.__len__()): # Verify the extension was found at the end
@@ -461,37 +461,37 @@ while True:
             print("Unable to determine image file extension from image URL")
             sys.exit()
 
-    # 8. PARSE IMAGE URL FOR FILENAME
+    # 9. PARSE IMAGE URL FOR FILENAME
     # NEW PROCEDURE FOR VERSION 1-2
     # get_image_filename(htmlString, [dateSearchPhrase], [nameSearchPhrase], nameEnding, skipDate=False)
     if imageURL.__len__() > 0:
-        ## 8.1. Get the unique filename suffix (does not include prefix or file extension)
+        ## 9.1. Get the unique filename suffix (does not include prefix or file extension)
         imageNameSuffix = get_image_filename(comicContentDecoded, dateSearchPhrase, nameSearchPhrase, nameEnding, skipDateIfNotFound)
 
         if imageNameSuffix == '00000000':
             print("Unable to determine a valid filename for the image!")
             sys.exit()
         else:
-            ## 8.2. Construct the local filename to save the image as
+            ## 9.2. Construct the local filename to save the image as
             incomingFilename = defaultFilename + imageNameSuffix + currentFileExtension
 
-            ## 8.3. Final filename trimming
+            ## 9.3. Final filename trimming
             incomingFilename = incomingFilename.replace('__','_')
             incomingFilename = incomingFilename.replace('--','-') 
 
-            ## 8.4. Verify the intended filename hasn't exceeded the OS maximum
+            ## 9.4. Verify the intended filename hasn't exceeded the OS maximum
             if incomingFilename.__len__() > MAX_FILENAME_LEN:
                 incomingFilename = incomingFilename[:MAX_FILENAME_LEN - currentFileExtension.__len__()] + currentFileExtension      
 
 #        print("Filename:\t{}".format(incomingFilename)) # DEBUGGING
 
-    # 9. DOWNLOAD THE FILE
+    # 10. DOWNLOAD THE FILE
     if imageURL.__len__() > 0 and incomingFilename.__len__() > 0:
-        ## 9.1. Verify the file doesn't exist so we're downloading it
+        ## 10.1. Verify the file doesn't exist so we're downloading it
         if os.path.exists(os.path.join(SAVE_PATH, incomingFilename)) == False:
-            ### 9.1.2. Check the robots.txt for permission, if applicable
+            ### 10.1.2. Check the robots.txt for permission, if applicable
             if obeyTheRobots is False or robots_may_I(page_disposition, imageURL) is True:
-                #### 9.1.2.1. Try to download it
+                #### 10.1.2.1. Try to download it
                 try:
                     # Utilizing a request-->urlopen-->write() in an attempt to...
                     # ...continue dodging websites that block webscrapers.
@@ -503,10 +503,10 @@ while True:
                 except urllib.error.HTTPError as error:
                     print("Image failed to download:\t{}".format(imageURL))
 
-                    #### 9.1.2.2. Handle 404 errors
+                    #### 10.1.2.2. Handle 404 errors
                     if error.code == 404:
                         num404Skips += 1
-                    #### 9.1.2.3. Abort on non 404 errors
+                    #### 10.1.2.3. Abort on non 404 errors
                     else:
                         print("ERROR:\t{} - {}".format(type(error),error))
                         sys.exit()
@@ -516,45 +516,45 @@ while True:
                     print(repr(error))
                     sys.exit()
 
-                #### 9.1.2.4. Success   
+                #### 10.1.2.4. Success   
                 else:
                     print("Image URL download successful:\t{}".format(incomingFilename)) # DEBUGGING
                     skipping = False
-            ### 9.1.3. Robots.txt forbids it
+            ### 10.1.3. Robots.txt forbids it
             else:
                 print("The site forbids our access to URL:\t{}".format(imageURL))
                 numExistingSkips += 1
                 skipping = True
 
-                ### 9.1.4. Some sites couple a Crawl-delay with a Disallow: /
+                ### 10.1.4. Some sites couple a Crawl-delay with a Disallow: /
                 if crawlDelay > 0:
                     # https://youtu.be/Udj-o2m39NA
                     print("Sleeping {} seconds before moving on".format(crawlDelay))
                     time.sleep(crawlDelay)
-        ## 9.2. The file exists so we're moving on
+        ## 10.2. The file exists so we're moving on
         else:
             print("Filename {} already exists.".format(incomingFilename)) # DEBUGGING
             numExistingSkips += 1
             skipping = True
 
-    # 10. CHECK IF WE'VE HIT THE END
-    ## 10.1. Current URL is actuall the first URL
+    # 11. CHECK IF WE'VE HIT THE END
+    ## 11.1. Current URL is actuall the first URL
     if currentURL == firstURL:                   # dynamically read First
         print("\nFinished scraping")
         break
-    ## 10.2. We've exceeded the maximum number of skips
+    ## 11.2. We've exceeded the maximum number of skips
     elif numExistingSkips >= MAX_EXISTING_SKIPS and MAX_EXISTING_SKIPS > 0:
         print("\n{} files already found.\nEnding scrape.".format(numExistingSkips))
         break
-    ## 10.3. This should only 'hit' if we start scraping on the first URL (see: Edge Case)
+    ## 11.3. This should only 'hit' if we start scraping on the first URL (see: Edge Case)
     elif firstURL.__len__() == 0 and firstSearchPhrase.__len__() > 0:
         print("\nMissing First URL.  We must be there.\nCurrent URL:\t{}\n".format(currentURL))
         break
-    ## 10.4. We've exceeded the tolerable number of 404 errors
+    ## 11.4. We've exceeded the tolerable number of 404 errors
     elif num404Skips >= MAX_404_SKIPS:
         print("\n{} 'Not Found (404)' errors encountered.\nEnding scrape.".format(num404Skips))
         break
-    ## 10.5. Penny Arcade edge case... First Comic URL different than Prev Comic URL on 2nd webpage
+    ## 11.5. Penny Arcade edge case... First Comic URL different than Prev Comic URL on 2nd webpage
     ## First URL == http://www.penny-arcade.com/comic/1998/11/18
     ## Prev URL == http://www.penny-arcade.com/comic/1998/11/18/the-sin-of-long-load-times
     ## Solution... find the First URL inside the Prev URL
@@ -563,15 +563,15 @@ while True:
         print("First URL:\t{}\nCurrent URL:\t{}".format(firstURL, currentURL)) # DEBUGGING
         break
 
-    # 11. PROCEED TO THE PREVIOUS PAGE
+    # 12. PROCEED TO THE PREVIOUS PAGE
     # NEW PROCEDURE FOR VERSION 1-2
     # find_a_URL(htmlString, [searchPhrase], [searchStart], [searchEnd])
-    ## 11.1. Get the URL associated with the 'Previous Comic' navigation button
+    ## 12.1. Get the URL associated with the 'Previous Comic' navigation button
     prevURL = find_a_URL(comicContentDecoded, prevSearchPhrase, 'href="', '"')
 
     prevURL = prevURL.replace('"', '') # find_a_URL() leaves the [searchEnd] on the return value
 
-    ## 11.2. Validate the previous URL
+    ## 12.2. Validate the previous URL
     if prevURL.__len__() > 0:
         ### 11.2.1. Change relative URLs to absolute URLs
         try:
@@ -589,7 +589,7 @@ while True:
         break
 
 
-    # 12. RESET TEMP VARIABLES TO AVOID DUPE DOWNLOADS AND OTHER ERRORS
+    # 13. RESET TEMP VARIABLES TO AVOID DUPE DOWNLOADS AND OTHER ERRORS
     incomingFilename = ''       # Local filename to save the incoming image download
     imageURL = ''               # Trimmed image URL
     currentFileExtension = ''   # File extension of current image to download
